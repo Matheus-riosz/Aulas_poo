f252716e02fede78f212ba60f63c762467c01e2c
matheus.mrios <mathesyy@gmail.com>
2025-10-10 20:56:04 -0300

    Aula 10/10/2025 Classes Abstratas

---
+++ Abstract/Main.java/src/FormaGeometrica.java
@@ -0,0 +1,22 @@
+public abstract class FormaGeometrica {
+    protected String cor;
+
+    public FormaGeometrica(String cor){
+        this.cor = cor;
+    }
+
+    public void exibirCor() {
+        System.out.println("A cor da Forma é: "+getCor());
+    }
+    public abstract double calcularArea();
+
+    public void exibirDetalhes(){
+        System.out.println("A cor da forma e: "+cor);
+        System.out.println(calcularArea());
+
+    }
+
+    public String getCor() {
+        return cor;
+    }
+}
